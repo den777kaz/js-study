4b81a814eecfa8b30c6771fd31904bed01e816d9
Denis Kazakov <k19den85@gmail.com>
2019-06-29 08:28:05 +0200

    complete hardlesson

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let num = 266219;
+    result = 2 * 6 * 6 * 2 * 1 * 9;
+    resultExponent = result ** 3; 
 
 
+console.log(result);
 
+console.log(result ** 3);
+
+console.log(String(resultExponent).slice(0,2));
