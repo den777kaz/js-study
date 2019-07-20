6aacdd0c958c7a03145a069043c8f5bce4025616
Denis Kazakov <k19den85@gmail.com>
2019-07-20 10:08:58 +0200

    fast complete l12

--- script/script.js
+++ script/script.js
@@ -1,41 +1,13 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
-    // timer
-    // Bad Code
-    // function countTimer (deadline){
-    //     let timerHours = document.querySelector('#timer-hours'),
-    //         timerMinutes = document.querySelector('#timer-minutes'),
-    //         timerSeconds = document.querySelector('#timer-seconds'),
-    //         dateStop = new Date(deadline).getTime(),
-    //         dateNow = new Date ().getTime(),
-    //         timeRemaining = (dateStop - dateNow) / 1000,
-    //         seconds = Math.floor(timeRemaining % 60),
-    //         minutes = Math.floor((timeRemaining / 60) % 60),
-    //         hours = Math.floor(timeRemaining / 60 / 60);
-    //         // esli nado dni vivesti ,  haurs nadi % 24 !!!!!!
-    //         // days = Math.floor(timeRemaining / 60 / 60 / 24);
-    //         // console.log('days: ', days);
-
-    //         timerHours.textContent = hours;
-    //         timerMinutes.textContent = minutes;
-    //         timerSeconds.textContent = seconds;
-
-
-    // }
-    // // countTimer('25 July 2019');
-
-    // setInterval(countTimer, 1000, '25 July 2019');
-
-    // good Code!!!
     function countTimer(deadline) {
 
         let timerHours = document.querySelector('#timer-hours'),
-            imerMinutes = document.querySelector('#timer-minutes'),
+            timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
 
         function getTimeRemaining() {
-
             let dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000,
@@ -43,10 +15,38 @@ window.addEventListener('DOMContentLoaded', function () {
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours = Math.floor(timeRemaining / 60 / 60);
 
+            return {
+                timeRemaining,
+                hours,
+                minutes,
+                seconds
+            };
         }
+
+        function updateClock() {
+            let timer = getTimeRemaining();
+
+            timerHours.textContent = ('0' + timer.hours).slice(-2);
+            timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+            timerSeconds.textContent = ('0' + timer.seconds).slice(-2);
+
+            if (timer.timeRemaining > 0) {
+                setInterval(updateClock, 1000);
+            }else {
+                clearInterval(updateClock);
+                timer.hours = '00';
+                timer.minutes = '00';
+                timer.seconds = '00';
+            }
+        }
+
+        updateClock();
+
     }
-
-
-
-
+    countTimer('21 July 2019');
 });
+
+
+// let today = new Date();
+// today.setHours(0, 0, 10, 0);
+// console.log('today: ', today);
